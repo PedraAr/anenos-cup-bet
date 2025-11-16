2bd27a0f5865bd94db9e47a02c662bf97344dade
PedraAr <126352398+PedraAr@users.noreply.github.com>
2025-11-16 18:14:42 -0500

    Initialize Firebase and export necessary functions

--- firebase.js
+++ firebase.js
@@ -1,1 +1,48 @@
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc4dw-ui7r4yAya3IOJV84gxNnrVG1aYw",
+  authDomain: "anenos-cup-bet.firebaseapp.com",
+  projectId: "anenos-cup-bet",
+  storageBucket: "anenos-cup-bet.firebasestorage.app",
+  messagingSenderId: "238398198636",
+  appId: "1:238398198636:web:ae93107f814cd403261949"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export {
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  onSnapshot,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
